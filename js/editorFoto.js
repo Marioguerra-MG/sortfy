7e0da9b9987f164e96b6a3c56021aea1782a6255
Mario <marioguerradasilvaneto93@gmail.com>
2025-03-09 18:48:05 -0300

    adicao do editor de imagem

---
+++ js/editorFoto.js
@@ -0,0 +1,158 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const newImg = document.getElementById('newImg');
+    const inputFile = document.querySelector("input[type=file]");
+    const img = document.querySelector(".img-content img");
+
+    const buttonsFilter = document.querySelectorAll(".filters-constent button");
+
+    const range = document.querySelector("input[type=range]");
+    const spnRageValue = document.getElementById('spnRangeValue');
+
+    const btnResetFilters = document.getElementById('btnResetFilters');
+    const btnSalvar = document.getElementById('btnSalvarImg');
+
+    let rotate, flipY, flipX;
+    let filterActive;
+    let filters;
+
+    btnResetFilters.onclick = () => init();
+
+    init();
+
+    function init() {
+        filters = {
+            Brilho: { value: 100, max: 200 },
+            Contraste: { value: 100, max: 200 },
+            Saturação: { value: 100, max: 200 },
+            Cinza: { value: 0, max: 100 },
+            Inversão: { value: 0, max: 100 },
+        };
+
+        rotate = 0;
+        flipY = 1;
+        flipX = 1;
+
+        filterActive = "Brilho";
+
+        spnRageValue.innerHTML = 100;
+        range.max = 200;
+        range.value = 100;
+
+        if (img) {
+            img.style.transform = "";
+            img.style.filter = "";
+        } else {
+            console.error("Erro: Elemento <img> não encontrado.");
+            return;
+        }
+
+        const activeButton = document.querySelector(".active");
+        if (activeButton) {
+            activeButton.classList.remove("active");
+        }
+
+        const filterDefault = document.getElementById('filterDefault');
+        if (filterDefault) {
+            filterDefault.classList.add("active");
+        }
+    }
+
+
+    newImg.onclick = () => inputFile.click();
+
+    inputFile.onchange = () => loadNewImage();
+
+    function loadNewImage(){
+        let file = inputFile.files[0];
+
+        if(file){
+            img.src = URL.createObjectURL(file);
+        }
+
+        init();
+    }
+
+
+    buttonsFilter.forEach((item) =>
+        item.onclick = () =>{
+            document.querySelector(".active").classList.remove('active');
+            item.classList.add("active");
+            filterActive = item.innerHTML;
+            range.max = filters[filterActive].max;
+            range.value = filters[filterActive].value;
+            spnRageValue.innerHTML = range.value;
+
+        }
+    )
+
+
+    range.oninput = () => {
+        filters[filterActive].value = range.value;
+        spnRageValue.innerHTML = range.value;
+
+        img.style.filter = `
+        brightness(${filters["Brilho"].value}%)
+        contrast(${filters["Contraste"].value}%)
+        saturate(${filters["Saturação"].value}%)
+        grayscale(${filters["Cinza"].value}%)
+        invert(${filters["Inversão"].value}%)
+    `;
+    }
+
+    function handleDirection(type) {
+        if (type === "rotateRight") {
+            rotate += 90;
+        } else if (type === "rotateLeft") {
+            rotate -= 90;
+        } else if (type === "reflectY") {
+            flipY = flipY === 1 ? -1 : 1;
+        } else {
+            flipX = flipX === 1 ? -1 : 1;
+        }
+
+        if (img) {
+            img.style.transform = `rotate(${rotate}deg) scale(${flipY}, ${flipX})`;
+        } else {
+            console.error("Erro: Elemento <img> não encontrado.");
+        }
+    }
+
+
+    btnSalvar.onclick = () => download();
+
+    function download(){
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+        canvas.width = img.naturalWidth;
+        canvas.height = img.naturalHeight;
+
+        ctx.filter = `
+        brightness(${filters["Brilho"].value}%)
+        contrast(${filters["Contraste"].value}%)
+        saturate(${filters["Saturação"].value}%)
+        grayscale(${filters["Cinza"].value}%)
+        invert(${filters["Inversão"].value}%)
+    `;
+
+    ctx.translate(canvas.width /2, canvas.height/2);
+        if(rotate !== 0 )ctx.rotate((rotate*Math.PI)/180);
+
+        ctx.scale(flipY ,flipX);
+        ctx.drawImage(
+            img,
+            -canvas.width / 2,
+            -canvas.height / 2,
+            canvas.width,
+            canvas.height
+        );
+
+        const link = document.createElement("a");
+        link.download = "Foto_Editada.png";
+        link.href = canvas.toDataURL();
+        link.click();
+    }
+
+
+
+    window.handleDirection = handleDirection;
+});
--- js/modal.js
+++ js/modal.js
@@ -5,80 +5,102 @@ const modal = document.getElementById('modal');
 
 // Função para abrir o modal
 openModalBtn.addEventListener('click', () => {
-    modal.style.display = 'flex'; // Exibe o modal (flex para centralizar)
+  modal.style.display = 'flex'; // Exibe o modal (flex para centralizar)
 });
 
 // Função para fechar o modal
 closeModalBtn.addEventListener('click', () => {
-    modal.style.display = 'none'; // Oculta o modal
+  modal.style.display = 'none'; // Oculta o modal
 });
 
 // Fechar o modal clicando fora do conteúdo
 window.addEventListener('click', (e) => {
-    if (e.target === modal) {
-        modal.style.display = 'none';
-    }
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
 });
 
-
 function editScheduledPost(schedule) {
-    let scheduledPosts = JSON.parse(localStorage.getItem('scheduledPosts')) || [];
-    const postToEdit = scheduledPosts.find(post => post.schedule === schedule);
-  
-    if (!postToEdit) return;
-  
-    // Preenche os campos do modal com os dados do agendamento
-    document.getElementById('modalPlatform').value = postToEdit.platform;
-    document.getElementById('modalTitulo').value = postToEdit.title;
-    document.getElementById('modalDescription').value = postToEdit.description;
-    document.getElementById('modalHashtags').value = postToEdit.hashtags;
-    document.getElementById('modalSchedule').value = postToEdit.schedule;
-  
-    // Exibe o modal
-    const modal = document.getElementById('editModal');
-    modal.style.display = "block";
-  
-    // Marca o formulário como "editando" e salva a referência ao agendamento
-    document.getElementById('modalForm').dataset.editing = schedule;
-    
-    // Fecha o modal quando o botão de fechar for clicado
-    document.querySelector('.close-btn').addEventListener('click', function() {
+  let scheduledPosts = JSON.parse(localStorage.getItem('scheduledPosts')) || [];
+  const postToEdit = scheduledPosts.find(post => post.schedule === schedule);
+
+  if (!postToEdit) return;
+
+  // Preenche os campos do modal com os dados do agendamento
+  document.getElementById('modalPlatform').value = postToEdit.platform;
+  document.getElementById('modalTitulo').value = postToEdit.title;
+  document.getElementById('modalDescription').value = postToEdit.description;
+  document.getElementById('modalHashtags').value = postToEdit.hashtags;
+  document.getElementById('modalSchedule').value = postToEdit.schedule;
+
+  // Exibe o modal
+  const modal = document.getElementById('editModal');
+  modal.style.display = "block";
+
+  // Marca o formulário como "editando" e salva a referência ao agendamento
+  document.getElementById('modalForm').dataset.editing = schedule;
+
+  // Fecha o modal quando o botão de fechar for clicado
+  document.querySelector('.close-btn').addEventListener('click', function () {
+    modal.style.display = "none";
+  });
+
+  // Fecha o modal quando o usuário clicar fora da área do modal
+  window.onclick = function (event) {
+    if (event.target === modal) {
       modal.style.display = "none";
-    });
-  
-    // Fecha o modal quando o usuário clicar fora da área do modal
-    window.onclick = function(event) {
-      if (event.target === modal) {
-        modal.style.display = "none";
+    }
+  };
+
+  // Lógica de submissão do formulário do modal
+  document.getElementById('modalForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const platform = document.getElementById('modalPlatform').value;
+    const title = document.getElementById('modalTitulo').value;
+    const description = document.getElementById('modalDescription').value;
+    const hashtags = document.getElementById('modalHashtags').value;
+    const schedule = document.getElementById('modalSchedule').value;
+
+    // Atualiza o agendamento específico
+    scheduledPosts = scheduledPosts.map(post => {
+      if (post.schedule === schedule) {
+        return { platform, title, description, hashtags, schedule };
       }
-    };
-  
-    // Lógica de submissão do formulário do modal
-    document.getElementById('modalForm').addEventListener('submit', function(event) {
-      event.preventDefault();
-  
-      const platform = document.getElementById('modalPlatform').value;
-      const title = document.getElementById('modalTitulo').value;
-      const description = document.getElementById('modalDescription').value;
-      const hashtags = document.getElementById('modalHashtags').value;
-      const schedule = document.getElementById('modalSchedule').value;
-  
-      // Atualiza o agendamento específico
-      scheduledPosts = scheduledPosts.map(post => {
-        if (post.schedule === schedule) {
-          return { platform, title, description, hashtags, schedule };
-        }
-        return post;
-      });
-  
-      // Salva os agendamentos atualizados
-      saveScheduledPosts(scheduledPosts);
-  
-      // Fecha o modal
-      modal.style.display = "none";
-  
-      // Atualiza a lista de agendamentos
-      reloadPostList();
+      return post;
     });
+
+    // Salva os agendamentos atualizados
+    saveScheduledPosts(scheduledPosts);
+
+    // Fecha o modal
+    modal.style.display = "none";
+
+    // Atualiza a lista de agendamentos
+    reloadPostList();
+  });
+}
+
+// Modal menu
+
+// Selecionando os elementos
+const openModalBtnMenu = document.getElementById('openModalBtnMenu');
+const closeModalBtnMenu = document.getElementById('closeModalBtnMenu');
+const modalMenu = document.getElementById('modalMenu');
+
+// Função para abrir o modal
+openModalBtnMenu.addEventListener('click', () => {
+  modalMenu.style.display = 'flex'; // Exibe o modal (flex para centralizar)
+});
+
+// Função para fechar o modal
+closeModalBtnMenu.addEventListener('click', () => {
+  modalMenu.style.display = 'none'; // Oculta o modal
+});
+
+// Fechar o modal clicando fora do conteúdo
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modalMenu.style.display = 'none';
   }
-  
+});
